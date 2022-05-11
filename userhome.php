be8d5375754a8aafe1eb284f7395ea51af17f923
RahuRabh <63224718+RahuRabh@users.noreply.github.com>
2022-05-11 23:49:35 +0530

    Add files via upload

---
+++ userhome.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/> 
+    <link rel="stylesheet" href="stu_style.css">  
+   
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <ul>
+                <li><a href="#">
+                <span class="nav-item">Dashboard</span></li>
+
+                <li><a href="userhome.php">
+                <i class="fa fa-home"></i>
+                <span class="nav-item">Home</span>
+                </a></li>
+
+                <li><a href="profile.php">
+                <i class="fas fa-user"></i>
+                <span class="nav-item">Profile</span>
+                </a></li>
+
+                <li><a href="#">
+                <i class="fa fa-chart-bar"></i>
+                <span class="nav-item">Courses</span>
+                </a></li>
+
+                <li><a href="#">
+                <i class="fas fa-cog"></i>
+                <span class="nav-item">Summary</span>
+                </a></li>
+           
+                <li><a href="logout.php" class="logout">
+                <i class="fas fa-sign-out-alt"></i>
+                <span class="nav-item">Logout</span>
+                </a></li>
+              
+            </ul>
+        </nav>
+
+        <section class="main">
+            <div class="main-top">
+                <h1>Skills</h1>
+                <i class="fas fa-user-cog"></i>
+            </div>
+            
+            <div class="main skills">
+                <div class="card">
+                    <i class="fas fa-laptop-code"></i>
+                    <h3>Web Development</h3>
+                    <p>Join the community</p>
+                    <button>Get started</button>
+                </div>
+
+                <div class="card">
+                    <i class="fas fa-laptop-code"></i>
+                    <h3>Web Development</h3>
+                    <p>Join the community</p>
+                    <button>Get started</button>
+                </div>
+
+                <div class="card">
+                    <i class="fas fa-laptop-code"></i>
+                    <h3>Web Development</h3>
+                    <p>Join the community</p>
+                    <button>Get started</button>
+                </div>
+
+                <div class="card">
+                    <i class="fas fa-laptop-code"></i>
+                    <h3>Web Development</h3>
+                    <p>Join the community</p>
+                    <button>Get started</button>
+                </div>
+
+            
+            </div>
+
+        
+        </section>
+    </div>
+    
+</body>
+</html> 
